c61af3148a8c277ad5eb0b4398e133c714a59f92
Pe1me4ka <52000143+Pe1me4ka@users.noreply.github.com>
2019-10-09 13:46:26 +0300

    Lab1

--- Lab1/Lab1/main.cpp
+++ Lab1/Lab1/main.cpp
@@ -11,8 +11,15 @@ int main() {
 	cin >> b;
 	cout << "input c: ";
 	cin >> c;
-	cout << "Space of your triangle is: " << TriangleSpace1(a, b, c) << endl;
-	cout << "Space of your triangle is: " << TriangleSpace2(a, b, c) << endl;
-	cout << "Space of your triangle is: " << TriangleSpace3(a, b, c) << endl;
+
+	cout << "\nSpace of your triangle is: " << TriangleSpace1(a, b, c) << endl;
+	cout << "Space of your triangle (with pointers) is: " << TriangleSpace2(&a, &b, &c) << endl;
+	cout << "Space of your triangle (with links) is: " << TriangleSpace3(a, b, c) << endl;
+
+	cout << "\nWhen a, b and c are const:" << endl;
+	const double x = a, y = b, z = c;
+	cout << "Space of your triangle is: " << TriangleSpace1(x, y, z) << endl;
+	cout << "Space of your triangle (with pointers) is: " << TriangleSpace22(&x, &y, &z) << endl;
+	cout << "Space of your triangle (with links) is: " << TriangleSpace33(x, y, z) << endl;
 	return 0;
 }
--- Lab1/Lab1/sq.cpp
+++ Lab1/Lab1/sq.cpp
@@ -15,7 +15,21 @@ static double TriangleSpace2(double *a, double *b, double *c) {
 	return S;
 }
 
+static double TriangleSpace22(const double* a, const double* b, const double* c) {
+	double S;
+	S = (*a + *b + *c) / 2;
+	S = sqrt(S * (S - *a) * (S - *b) * (S - *c));
+	return S;
+}
+
 static double TriangleSpace3(double &a, double &b, double &c) {
+	double S;
+	S = (a + b + c) / 2;
+	S = sqrt(S * (S - a) * (S - b) * (S - c));
+	return S;
+}
+
+static double TriangleSpace33(const double& a, const double& b, const double& c) {
 	double S;
 	S = (a + b + c) / 2;
 	S = sqrt(S * (S - a) * (S - b) * (S - c));
